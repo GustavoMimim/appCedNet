98fe8da2ecb9eca12ed6219c2ede4e59ebe6fb60
gustavo <gustavo-bergamo@live.com>
2020-07-06 13:41:12 -0300

    Dark map e localizações de ténicos

--- src/views/map/Map.js
+++ src/views/map/Map.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import { Text, View, StyleSheet, StatusBar } from 'react-native';
+import { Text, View, StyleSheet, StatusBar, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+
+import mapStyle from './mapStyle'
+
+
+const positionTechnical = [
+  {
+    latitude: -23.040826,
+    longitude: -49.703366,
+    title: 'Sua localização',
+    description: 'Ford Ka - 2020'
+  },
+  {
+    latitude: -22.9931817, 
+    longitude: -49.8638002,
+    title: 'Vinicius A. Ribeiro',
+    description: 'Mobi - 2020'
+  }
+]
+
 
 export default function Map () {
   return (
     <View style={styles.container}>
-      <StatusBar hidden={false} barStyle = "dark-content" backgroundColor="white" translucent />
+      <StatusBar hidden={false} barStyle="dark-content" backgroundColor="white" translucent />
       <MapView
+        pitchEnabled={true}
+        showsCompass={true}
+        liteMode={false}
+        showsBuildings={true}
+        showsTraffic={true}
+        showsIndoors={true}
         showsUserLocation={true}
         showsMyLocationButton={true}
-        style={styles.image}
+        customMapStyle={mapStyle}
+        style={styles.map}
         initialRegion={{
           latitude: -23.040826,
           longitude: -49.703366,
@@ -17,15 +43,28 @@ export default function Map () {
           longitudeDelta: 0.0421
         }}
       >
-        <Marker
-          coordinate={{            
-            latitude: -23.040826,
-            longitude: -49.703366,
-          }}
-          title='Sua localização'
-          description='Ford Ka - 2020'
+        
+        {
+          positionTechnical.map((u, i) => {
+            return (
+              <Marker
+                key={i}
+                coordinate={{
+                  latitude: u.latitude,
+                  longitude: u.longitude,
+                }}
+                title={u.title}
+                description={u.description}
+                icon={{
+                  uri: "https://img.icons8.com/color/72/000000/fiat-500.png"
+                }}
 
-        />
+              />
+            );
+          })
+        }
+
+
       </MapView>
     </View>
   );
@@ -33,8 +72,13 @@ export default function Map () {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  image: {
-    flex: 1,
+  map: {
+    width: Dimensions.get('window').width,
+    top: 25,
+    height: '100%'
   },
 })
